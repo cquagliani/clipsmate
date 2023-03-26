0e40b80f4091bf27c1a596befed8dc585fb6fb01
Chris Quagliani <chrisquag24@berkeley.edu>
2023-03-25 20:56:41 -0700

    Create page for the core product

---
+++ pages/dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div className="flex justify-center items-center h-screen w-screen bg-blue-100">
+            <p>Login successful. Welcome to your dashboard!</p>
+        </div>
+    );
+}
+
+export default Dashboard;
