42ca5226459692d23027b51ec0841f73c10acee2
Chris Quagliani <chrisquag24@berkeley.edu>
2023-03-25 23:22:45 -0700

    Create basic layout with placeholders

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,11 +1,28 @@
 import DashboardHeader from "@component/components/dashboardHeader";
+import ListItem from "@component/components/listItem";
+import GhostListItem from "@component/components/ghostListItem";
+import AddButton from "@component/components/addButton";
 
 const Dashboard = () => {
     return (
         <div>
             <DashboardHeader />
-            <div className="flex justify-center items-center h-screen w-screen bg-blue-100">
-                <p>Login successful. Welcome to your dashboard!</p>
+            <div className="h-screen w-screen bg-blue-100 px-[10%] py-24">
+                <div className="flex flex-col justify-start">
+                    <div className="flex flex-row justify-between px-8">
+                        <h1 className="font-bold text-3xl align-left">Dashboard</h1>
+                        <AddButton />
+                    </div>
+                    <div className="flex justify-stretch items-center">
+                        <div className="flex flex-col gap-4 border border-solid border-black rounded-3xl p-10 w-full h-full mt-12 bg-gray-100 bg-opacity-60 min-w-fit">
+                            <ListItem />
+                            <ListItem />
+                            <ListItem />
+                            <ListItem />
+                            <GhostListItem />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
