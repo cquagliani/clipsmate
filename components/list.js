260acdd67cf2289da4c22815a5695873f01ba2b3
Chris Quagliani <chrisquag24@berkeley.edu>
2023-03-26 13:25:50 -0700

    Create component to render all list content from db

---
+++ components/list.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import ListItem from './listItem';
+import GhostListItem from "./ghostListItem";
+import {  query, collection, onSnapshot, updateDoc, getDocs, doc, addDoc, deleteDoc, } from 'firebase/firestore';
+import { db } from "../firebase/clientApp";
+
+function List() {
+    const [list, setList] = useState([]);
+    const colRef = collection(db, 'list');
+
+    // Read list items from database
+    useEffect(() => {
+        const getList = async () => {
+            const data = await getDocs(colRef);
+            setList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        }
+        getList();
+      }, []);
+
+    return (
+        <div className="flex items-center">
+            <ul id="itemsList" className="flex flex-col gap-4 border border-solid border-black rounded-3xl p-10 w-full h-full mt-12 bg-gray-100 bg-opacity-60 min-w-fit">
+                {list.map((item) => (
+                    <ListItem listLabel={item.label} listItem={item.text}/>
+                ))}
+                <GhostListItem />
+            </ul>
+        </div>
+    )
+}
+
+export default List
