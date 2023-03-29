bce51f896e355b555b034f01f7ddd84c94a7dad5
Chris Quagliani <chrisquag24@berkeley.edu>
2023-03-29 00:58:00 -0700

    Adjust styling

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,15 +1,16 @@
 import DashboardHeader from "@component/components/dashboardHeader";
 import List from '@component/components/list'
 import ProtectedRoute from "../components/protectedRoute";
+import AccordionComponent from "@component/components/accordion";
 
 const Dashboard = () => {
     return (
         <ProtectedRoute>
             <div>
                 <DashboardHeader />
-                <div className="h-screen w-screen bg-blue-100 px-[10%] py-24">
+                <div className="h-screen w-screen px-[10%] py-24">
                     <div className="flex flex-col justify-start">
-                        <div className="flex flex-row justify-between px-8">
+                        <div className="flex flex-row justify-between px-2">
                             <h1 className="font-bold text-3xl align-left">Dashboard</h1>
                         </div>
                         <List />
