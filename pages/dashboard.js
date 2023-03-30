b858bdee1fab13673a15410cdfec400a4f47dcb6
Chris Quagliani <chrisquag24@berkeley.edu>
2023-03-29 18:33:00 -0700

    Style with MUI grid

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,7 @@
 import DashboardHeader from "@component/components/dashboardHeader";
 import List from '@component/components/list'
 import ProtectedRoute from "../components/protectedRoute";
-import AccordionComponent from "@component/components/accordion";
+import Grid from '@mui/material/Grid'
 
 const Dashboard = () => {
     return (
@@ -9,12 +9,20 @@ const Dashboard = () => {
             <div>
                 <DashboardHeader />
                 <div className="h-screen w-screen px-[10%] py-24">
-                    <div className="flex flex-col justify-start">
-                        <div className="flex flex-row justify-between px-2">
-                            <h1 className="font-bold text-3xl align-left">Dashboard</h1>
-                        </div>
-                        <List />
-                    </div>
+                <h1 className="font-bold text-3xl align-left">Dashboard</h1>
+                    <Grid container spacing={4} direction="row" justifyContent="center">
+                        <Grid item xs={8}>
+                            <List title="My Clips" />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <List title="My Tasks"/>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={4} direction="row" justifyContent="" alignItems="center">
+                        <Grid item xs={12}>
+                            <List title="Clipboard History"/>
+                        </Grid>
+                    </Grid>
                 </div>
             </div>
         </ProtectedRoute>
