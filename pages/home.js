124c58d309aab386d442b979d9895c208f66ba90
Chris Quagliani <chrisquag24@berkeley.edu>
2023-04-02 21:26:58 -0700

    Redo header, hero sections

--- pages/home.js
+++ pages/home.js
@@ -3,26 +3,19 @@ import HeaderComponent from '@component/components/header';
 
 const Home = () => {
     return (
-      <div className="bg-slate-200 pb-12">
+      <div className="bg-[#FCFCFC] pb-12 h-[300vh]">
         <HeaderComponent />
-        <div className="flex justify-center items-start py-[15%] h-[60%] bg-slate-300">
-          <div className="w-full flex flex-row items-start justify-between px-[15%]">
-            <div className="flex flex-col items-start justify-center">
-              <h1 className="font-bold text-5xl">Your Clips. Your Tasks. <br /> All in One Place.</h1>
-              <p className="font-light py-2 px-1 text-lg">Save time and stay organized with our newly designed dashboard.</p>
-            </div>
-            <div className="flex flex-col items-start justify-center">
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-row gap-12 px-[15%] py-[5%]">
-          <div className="flex flex-col border-2 border-solid border-black rounded-2xl h-[350px] w-[300px]"></div>
-          <div className="flex flex-col border-2 border-solid border-black rounded-2xl h-[350px] w-[300px]"></div>
-          <div className="flex flex-col border-2 border-solid border-black rounded-2xl h-[350px] w-[300px]"></div>
-        </div>
-        <div className="flex justify-center items-center">
-          <button className="bg-blue-900 text-white border border-black rounded-2xl py-4 px-12">Learn More</button>
-        </div>
+
+        {/* HERO SECTION */}
+        <section className="flex flex-col items-center justify-start sm:justify-center pt-[50%] sm:pt-[0%] w-full h-screen px-16">  
+            <h1 className="font-bold text-6xl text-center leading-[5rem]">Your Clips. Your Tasks. <br /> All in One Place.</h1>
+            <p className="w-[20rem] sm:w-[25rem] text-center mt-6">Lorem ipsum dolor sit amet consectetur adipiscing elit, class aliquam nullam aenean sodales primis eleifend, viverra non eros magnis tincidunt sagittis.</p>
+        </section>
+
+        {/* DEMO SECTION */}
+        <section className="-mt-20 px-16">
+          <div className="border-4 border-black bg-[#0F3460] rounded-3xl h-[1000px]"></div>
+        </section>
       </div>
     );
   };
