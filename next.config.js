ebd06b57b096a5c5225b0aed66bfbf03a2e79ac5
Chris Quagliani <chrisquag24@berkeley.edu>
2023-12-12 23:12:49 -0800

    Deploy with GitHub pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  output: "export",
+  basePath: "/clipsmate",
+  images: { unoptimized: true } 
 }
 
 module.exports = nextConfig
